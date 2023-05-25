1f195dfbfbc51493cb908c02423602482eb39f0f
Rustem Zhantemirov <zhantemirov.rustem@gmail.com>
2023-05-25 17:03:32 +0600

    UPD: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoinExchangeModule } from './coin-exchange/coin-exchange.module';
 
 @Module({
-  imports: [CoinExchangeModule],
+  imports: [
+    CoinExchangeModule,
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 60000, // milliseconds
+      max: 100,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
