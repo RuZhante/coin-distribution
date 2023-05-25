73ea21230d3074b6a9ce0bf8eaddd12f73e233d5
Rustem Zhantemirov <zhantemirov.rustem@gmail.com>
2023-05-25 17:04:04 +0600

    FIX: coin-converter

--- src/utils/coin-converter.spec.ts
+++ src/utils/coin-converter.spec.ts
@@ -1,90 +1,41 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { coinConverter } from './coin-converter';
 import { faker } from '@faker-js/faker';
 
-describe('Checking the coinConverter function', () => {
-  const testCases = [
-    {
-      title: 'the amount is less than or equal to 0',
-      amount: faker.datatype.number({ min: 0, max: 1000 }) * -1,
-      fromPrice: faker.datatype.number({ min: 1, max: 1000 }),
-      toPrice: faker.datatype.number({ min: 1, max: 1000 }),
-      expected: 0,
-    },
-    {
-      title: 'the fromPrice is less than or equal to 0',
-      amount: faker.datatype.number({ min: 1, max: 1000 }),
-      fromPrice: faker.datatype.number({ min: 0, max: 1000 }) * -1,
-      toPrice: faker.datatype.number({ min: 1, max: 1000 }),
-      expected: 0,
-    },
-    {
-      title: 'the toPrice is less than or equal to 0',
-      amount: faker.datatype.number({ min: 1, max: 1000 }),
-      fromPrice: faker.datatype.number({ min: 0, max: 1000 }),
-      toPrice: faker.datatype.number({ min: 0, max: 1000 }) * -1,
-      expected: 0,
-    },
-  ];
-
-  const testCases2 = [
-    {
-      title: 'the fromPrice is more to toPrice',
-      amount: faker.datatype.number({ min: 1, max: 1000 }),
-      fromPrice: faker.datatype.number({ min: 1001, max: 2000 }),
-      toPrice: faker.datatype.number({ min: 1, max: 1000 }),
-    },
-    {
-      title: 'the toPrice is more to fromPrice',
-      amount: faker.datatype.number({ min: 1, max: 1000 }),
-      fromPrice: faker.datatype.number({ min: 1, max: 1000 }),
-      toPrice: faker.datatype.number({ min: 1001, max: 2000 }),
-    },
-  ];
-
-  const arr = [null, undefined, Infinity];
-
-  const testCases3 = [
-    {
-      title: 'the amount undefined, null or Infinity',
-      amount: arr[Math.floor(Math.random() * arr.length)],
-      fromPrice: faker.datatype.number({ min: 1001, max: 2000 }),
-      toPrice: faker.datatype.number({ min: 1, max: 1000 }),
-      expected: 0,
-    },
-    {
-      title: 'the fromPrice undefined, null or Infinity',
-      amount: faker.datatype.number({ min: 1, max: 1000 }),
-      fromPrice: arr[Math.floor(Math.random() * arr.length)],
-      toPrice: faker.datatype.number({ min: 1, max: 1000 }),
-      expected: 0,
-    },
-    {
-      title: 'the toPrice undefined, null or Infinity',
-      amount: faker.datatype.number({ min: 1, max: 1000 }),
-      fromPrice: faker.datatype.number({ min: 1, max: 1000 }),
-      toPrice: arr[Math.floor(Math.random() * arr.length)],
-      expected: 0,
-    },
-  ];
-
-  testCases.forEach((test) => {
-    it(`${test.title}`, () => {
-      const result = coinConverter(test.fromPrice, test.toPrice, test.amount);
-      expect(result).toBe(test.expected);
-    });
+describe('coinConverter function', () => {
+  it('should throw an error if arguments are not of type BigInt', () => {
+    // @ts-ignore
+    // tslint:disable-next-line
+    expect(() => coinConverter(1, 2, 3)).toThrow(TypeError);
+    // @ts-ignore
+    // tslint:disable-next-line
+    expect(() => coinConverter('1n', '2n', '3n')).toThrow(TypeError);
+    // @ts-ignore
+    // tslint:disable-next-line
+    expect(() => coinConverter(null, undefined, Infinity)).toThrow(TypeError);
   });
 
-  testCases2.forEach((test) => {
-    it(`${test.title}`, () => {
-      const result = coinConverter(test.fromPrice, test.toPrice, test.amount);
-      expect(result).toBeGreaterThan(0);
-    });
+  it('should throw a range error if any of the arguments are <= 0', () => {
+    expect(() => coinConverter(0n, 1n, 1n)).toThrow(RangeError);
+    expect(() => coinConverter(1n, 0n, 1n)).toThrow(RangeError);
+    expect(() => coinConverter(1n, 1n, 0n)).toThrow(RangeError);
   });
 
-  testCases3.forEach((test) => {
-    it(`${test.title}`, () => {
-      const result = coinConverter(test.fromPrice, test.toPrice, test.amount);
-      expect(result).toBe(test.expected);
-    });
+  it('should convert coins correctly', () => {
+    // Use faker to generate random numbers for testing
+    const fromPrice = BigInt(faker.datatype.number({ min: 1, max: 1000000 }));
+    const toPrice = BigInt(faker.datatype.number({ min: 1, max: 1000000 }));
+    const amount = BigInt(faker.datatype.number({ min: 1, max: 1000000 }));
+    const expected = ((amount * toPrice) / fromPrice).toString();
+
+    const result = coinConverter(fromPrice, toPrice, amount);
+    expect(result).toBe(expected);
+  });
+
+  it('should throw an error if the result is not a finite number', () => {
+    // This test case should throw an error because the result is too large to be a finite number
+    // @ts-ignore
+    // tslint:disable-next-line
+    expect(() => coinConverter(1n, 1n, Number.MAX_SAFE_INTEGER)).toThrow(Error);
   });
 });
--- src/utils/coin-converter.ts
+++ src/utils/coin-converter.ts
@@ -1,27 +1,35 @@
-export const coinConverter = (
-  fromPrice: number,
-  toPrice: number,
-  amount: number,
-) => {
+export function coinConverter(
+  fromPrice: bigint,
+  toPrice: bigint,
+  amount: bigint,
+): string {
   if (
-    toPrice === null ||
-    toPrice === undefined ||
-    toPrice === Infinity ||
-    toPrice === -Infinity ||
-    toPrice <= 0 ||
-    amount === null ||
-    amount === undefined ||
-    amount === Infinity ||
-    amount === -Infinity ||
-    amount <= 0 ||
-    fromPrice === null ||
-    fromPrice === undefined ||
-    fromPrice === Infinity ||
-    fromPrice === -Infinity ||
-    fromPrice <= 0
+    typeof fromPrice !== 'bigint' ||
+    typeof toPrice !== 'bigint' ||
+    typeof amount !== 'bigint'
   ) {
-    return 0;
+    throw new TypeError('Неверный тип аргументов. Ожидаемый тип: BigInt');
   }
 
-  return parseFloat(Number((fromPrice * amount) / toPrice).toFixed(1));
-};
+  if (fromPrice <= 0n || toPrice <= 0n || amount <= 0n) {
+    throw new RangeError('Аргументы должны быть больше нуля.');
+  }
+
+  // Умножаем цену за единицу конвертируемой криптовалюты (toPrice) на количество конвертируемых единиц (amount),
+  // что даст нам общую стоимость в центах.
+  const totalAmount = toPrice * amount;
+
+  // Вычисляем результат деления общей стоимости (totalAmount) на цену за единицу исходной криптовалюты (fromPrice).
+  // Мы используем BigInt-арифметику, чтобы сохранить максимальную точность вычислений.
+  const result = totalAmount / fromPrice;
+
+  // Проверяем, является ли результат конечным числом, с помощью метода isFinite().
+  if (!isFinite(Number(result))) {
+    throw new Error(
+      'Ошибка конвертации. Результат не является конечным числом.',
+    );
+  }
+
+  // Преобразуем результат в строку с помощью метода toString(), чтобы сохранить максимальную точность.
+  return result.toString();
+}
