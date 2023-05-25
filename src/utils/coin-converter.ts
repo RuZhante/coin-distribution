67dd07464142e8c5ac96d06ca1c69d3341c7b1c0
Rustem Zhantemirov <zhantemirov.rustem@gmail.com>
2023-05-25 17:18:20 +0600

    UPD: coin-converter.ts

--- src/utils/coin-converter.ts
+++ src/utils/coin-converter.ts
@@ -1,35 +1,44 @@
+/**
+ * Converts a certain amount of cryptocurrency from one type to another based on their respective prices.
+ * @param {bigint} fromPrice - The price of one unit of the initial cryptocurrency.
+ * @param {bigint} toPrice - The price of one unit of the desired cryptocurrency.
+ * @param {bigint} amount - The amount of cryptocurrency to convert.
+ * @returns {string} - The converted amount of cryptocurrency in the desired type, with maximum precision.
+ * @throws {TypeError} - If any of the arguments is not of type BigInt.
+ * @throws {RangeError} - If any of the arguments is less than or equal to zero.
+ * @throws {Error} - If the result of the conversion is not a finite number.
+ */
 export function coinConverter(
   fromPrice: bigint,
   toPrice: bigint,
   amount: bigint,
 ): string {
+  // Check if all arguments are of type BigInt, and throw an error if not.
   if (
     typeof fromPrice !== 'bigint' ||
     typeof toPrice !== 'bigint' ||
     typeof amount !== 'bigint'
   ) {
-    throw new TypeError('Неверный тип аргументов. Ожидаемый тип: BigInt');
+    throw new TypeError('Invalid argument type. Expected type: BigInt');
   }
 
+  // Check if any of the arguments is less than or equal to zero, and throw an error if so.
   if (fromPrice <= 0n || toPrice <= 0n || amount <= 0n) {
-    throw new RangeError('Аргументы должны быть больше нуля.');
+    throw new RangeError('Arguments must be greater than zero.');
   }
 
-  // Умножаем цену за единицу конвертируемой криптовалюты (toPrice) на количество конвертируемых единиц (amount),
-  // что даст нам общую стоимость в центах.
+  // Multiply the price per unit of the desired cryptocurrency by the amount of cryptocurrency to convert,
+  // which gives us the total value.
   const totalAmount = toPrice * amount;
 
-  // Вычисляем результат деления общей стоимости (totalAmount) на цену за единицу исходной криптовалюты (fromPrice).
-  // Мы используем BigInt-арифметику, чтобы сохранить максимальную точность вычислений.
+  // Divide the total value (totalAmount) by the price per unit of the initial cryptocurrency (fromPrice).
   const result = totalAmount / fromPrice;
 
-  // Проверяем, является ли результат конечным числом, с помощью метода isFinite().
+  // Check if the result is a finite number using the isFinite() method, and throw an error if not.
   if (!isFinite(Number(result))) {
-    throw new Error(
-      'Ошибка конвертации. Результат не является конечным числом.',
-    );
+    throw new Error('Conversion error. The result is not a finite number.');
   }
 
-  // Преобразуем результат в строку с помощью метода toString(), чтобы сохранить максимальную точность.
+  // Convert the result to a string using the toString() method, to ensure maximum precision.
   return result.toString();
 }
